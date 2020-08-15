be91e6b34c15f6fe6ccdbd678feabdaa26332511
lanyanyan <bupt.dzh@gmail.com>
2020-08-15 14:49:19 +0800

    找出数组中重复数字 const 版本 和 non-const版本 前者对范围采用二分法，遍历计数 后者利用已知条件实现"部分排序" const 版本的二分算法值得琢磨

--- offer/ch02/DuplicateNumber.cpp
+++ offer/ch02/DuplicateNumber.cpp
@@ -8,7 +8,7 @@
 #include<vector>
 #include<iostream>
 
-int DuplicateNumber(std::vector<int> nums)
+int DuplicateNumber(std::vector<int>& nums)
 {
     const int ERROR = -1;
     const int len = nums.size();
@@ -47,10 +47,63 @@ int DuplicateNumber(std::vector<int> nums)
     return ERROR;
 }
 
+int countRange(const std::vector<int>& nums, int min, int max)
+{
+    if (! nums.size())
+        return 0;
+
+    int count = 0;
+    for (const auto& x : nums)
+        if (min <= x && max >= x)
+            ++count;
+    
+    return count;
+}
+
+// 要求不修改数组的情况下找出重复元素
+int DuplicateNumber(const std::vector<int>& nums)
+{
+    const int ERROR = -1;
+    const int len = nums.size();
+    if (!len) return ERROR;
+
+    // 已知 nums[i] < n, 但我们不能完全信任传进来的参数
+    for (const auto& x : nums)
+        if (x < 1 || x > len-1)
+            return ERROR;
+
+    int l = 1;
+    int r = nums.size()-1;
+    int m;
+
+    // 划分的是1~n这个范围
+    while (l < r)
+    {
+        m = l + ((r-l) >> 1);
+
+        printf("l: %d, r: %d, m: %d.\n", l, r, m);
+
+        if (countRange(nums, l, m) > m - l + 1)
+            r = m;
+        else if (countRange(nums, m+1, r) > r - m)
+            l = m+1;
+        else
+            return -1; 
+    }
+
+    // l=m=r
+    if (l == r)
+        if (countRange(nums, l, l+((r-l)>>1)) > 1)
+                return l;
+
+    return -1;
+
+}
+
 void test()
 {
-    std::vector<int> nums{ 1, 0, 9, 3, 6, 9, 3, 2, 8, 1 }; // 有重复
-    // std::vector<int> nums{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 无重复
+    // std::vector<int> nums{ 1, 0, 9, 3, 6, 9, 3, 2, 8, 1 }; // 有重复
+    const std::vector<int> nums{ 1, 2, 3, 4, 5, 6, 7, 8, 3 }; // 无重复
     // std::vector<int> nums{ -1, 1, 2, 3, 4, 5, 6, 7, 8, 9 }; 无效输入
     // std::vector<int> nums{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 10 }; 无效输入
     std::cout << DuplicateNumber(nums) << std::endl;
